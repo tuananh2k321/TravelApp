53e923748365483d30ca68ff6bbdfd8a9cf9879b
trieunnh5903 <111626907+trieunnh5903@users.noreply.github.com>
2023-05-17 00:31:54 +0700

    cap nhat bottomtab

--- screen/BottomTab.js
+++ screen/BottomTab.js
@@ -10,58 +10,69 @@ import Profile from "./TabApp/Profile";
 const Tab = createBottomTabNavigator();
 
 const BottomTab = () => {
-    return (
-        <Tab.Navigator
-          screenOptions={
-            ({ route }) => ({
-              tabBarIcon: ({ focused, label }) => {
-    
-                let iconName = focused
-                if (route.name === 'Home') {
-                  iconName = focused ? ICON.home_d : ICON.home
-                  label = 'Home'
-                } else if (route.name === 'Favorite') {
-                  iconName = focused ? ICON.heart_d : ICON.heart;
-                  label = 'Favorites'
-                } else if (route.name === 'Notification') {
-                  iconName = focused ? ICON.notification_d : ICON.notification;
-                  label = 'Notification'
-                } else if (route.name === 'Profile') {
-                  iconName = focused ? ICON.user_d : ICON.user;
-                  label = 'Profile'
-                }
-                
-                // You can return any component that you like here!
-                return <View style={{
-                  flex: 1,
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  width: 80
-                }}>
-                  <Image source={iconName}
-                    style={{
-                      width: 30, height: 30, resizeMode: 'stretch',
-                      tintColor: focused ? COLOR.primary : COLOR.detail
-                    }} />
-                  <Text style={{
-                    color: focused ? COLOR.primary : COLOR.detail,
-    
-                  }}>{label}</Text>
-                </View>;
-              },
-              headerShown: false,
-              tabBarShowLabel: false,
-              tabBarStyle: {
-                height: 70,
-              },
-            })}
-        >
-          <Tab.Screen name="Home" component={Home} />
-          <Tab.Screen name="Favorite" component={Favorite} />
-          <Tab.Screen name="Notification" component={Notification} />
-          <Tab.Screen name="Profile" component={Profile} />
-        </Tab.Navigator>
-      );
+  return (
+    <Tab.Navigator
+      screenOptions={
+        ({ route }) => ({
+          tabBarIcon: ({ focused, label }) => {
+
+            let iconName = focused
+            if (route.name === 'Home') {
+              iconName = ICON.home
+              label = 'Home'
+            } else if (route.name === 'Favorite') {
+              iconName = ICON.heart_d;
+              label = 'Favorite'
+            } else if (route.name === 'Notification') {
+              iconName = ICON.notification_d;
+              label = 'Notification'
+            } else if (route.name === 'Profile') {
+              iconName = ICON.user;
+              label = 'Profile'
+            }
+
+            // You can return any component that you like here!
+            return <View style={{
+              flex: 1,
+              alignItems: 'center',
+              justifyContent: 'center',
+              width: 80,
+              position: 'absolute',
+              bottom: 0
+            }}>
+              <Image source={iconName}
+                style={{
+                  width: 30, height: 30, resizeMode: 'stretch',
+                  tintColor: focused ? COLOR.primary : COLOR.detail
+                }} />
+              <Text style={{
+                color: focused ? COLOR.primary : COLOR.detail,
+
+              }}>{label}</Text>
+
+              <View style={{
+                width: '80%',
+                height: 4,
+                marginTop: 5,
+                borderRadius: 48,
+                backgroundColor: focused ? COLOR.primary : null
+              }} />
+
+            </View>;
+          },
+          headerShown: false,
+          tabBarShowLabel: false,
+          tabBarStyle: {
+            height: 67,
+          },
+        })}
+    >
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Favorite" component={Favorite} />
+      <Tab.Screen name="Notification" component={Notification} />
+      <Tab.Screen name="Profile" component={Profile} />
+    </Tab.Navigator>
+  );
 }
 
 export default BottomTab
