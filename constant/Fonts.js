1996e79dfbcedcc9f146586b079ad6078cbfa32d
trieunnh5903 <111626907+trieunnh5903@users.noreply.github.com>
2023-05-18 11:05:29 +0700

    THÊM FONT

---
+++ constant/Fonts.js
@@ -0,0 +1,15 @@
+import { SIZES } from "./Themes";
+
+export const FONTS = {
+    largeTitle: { fontFamily: "Roboto-Black", fontSize: SIZES.largeTitle },
+    h1: { fontFamily: "Roboto-Bold", fontSize: SIZES.h1 },
+    h2: { fontFamily: "Roboto-Bold", fontSize: SIZES.h2 },
+    h3: { fontFamily: "Roboto-SemiBold", fontSize: SIZES.h3 },
+    h4: { fontFamily: "Roboto-SemiBold", fontSize: SIZES.h4 },
+    h5: { fontFamily: "Roboto-SemiBold", fontSize: SIZES.h5 },
+    body1: { fontFamily: "Roboto-Regular", fontSize: SIZES.body1 },
+    body2: { fontFamily: "Roboto-Regular", fontSize: SIZES.body2 },
+    body3: { fontFamily: "Roboto-Regular", fontSize: SIZES.body3 },
+    body4: { fontFamily: "Roboto-Regular", fontSize: SIZES.body4 },
+    body5: { fontFamily: "Roboto-Regular", fontSize: SIZES.body5 },
+};
