81d5758fe2d39d3aeb2a18ebabcc66e9cf43d385
tuananh2k321 <haizzj123@gmail.com>
2023-09-27 22:31:46 +0700

    TA done api login

--- constant/AxiosIntance.js
+++ constant/AxiosIntance.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 const AxiosIntance = (contentType = 'application/json') => {
     const axiosInstance = axios.create({
-        baseURL: 'http://192.168.1.4/'
+        baseURL: 'http://192.168.1.4:3000/'
     });
     axiosInstance.interceptors.request.use(
-        async config => { 
+        async config => {
             const token = await AsyncStorage.getItem('token');
             config.headers = {
                 'Authorization': `Bearer ${token}`,
@@ -23,4 +23,4 @@ const AxiosIntance = (contentType = 'application/json') => {
     return axiosInstance;
 }
 
-export default AxiosIntance
+export default AxiosIntance;
