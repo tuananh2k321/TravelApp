0288d78a819bf318264f1bf7578468b15bfd6f95
tuanbasketball-2003 <nguyenductuanff2003@gmail.com>
2023-10-28 18:55:18 +0700

    up

--- screen/tab_app/Favorite/Favorite.js
+++ screen/tab_app/Favorite/Favorite.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View,FlatList,TouchableOpacity,Image } from 'react-native'
+import { StyleSheet, Text, View, FlatList, TouchableOpacity, Image } from 'react-native'
 import { SwipeListView } from 'react-native-swipe-list-view'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
@@ -9,21 +9,21 @@ import Item_wishlist from '../../../component/Tab_item/Item_wishlist'
 
 
 const Favorite = (props) => {
-    const {navigation} = props;
+  const { navigation } = props;
   return (
     <SafeAreaView style={styles.container}>
-        <Text style={styles.wishlist}>Yêu thích</Text>
-        <View style={styles.wishlist_list}>
-            <FlatList style={{bottom:20}}
-        data={data}
-        contentContainerStyle={{ flexGrow: 1 }}
-        renderItem={({item}) => <Item_wishlist dulieu={item} navigation={navigation}/>}
-        keyExtractor={item => item.id}
-        showsVerticalScrollIndicator={false}
-        removeClippedSubviews={true}
-      >
-      </FlatList>
-      {/* <SwipeListView
+      <Text style={styles.wishlist}>Yêu thích</Text>
+      <View style={styles.wishlist_list}>
+        <FlatList style={{ bottom: 20 }}
+          data={data}
+          contentContainerStyle={{ flexGrow: 1 }}
+          renderItem={({ item }) => <Item_wishlist dulieu={item} navigation={navigation} />}
+          keyExtractor={item => item.id}
+          showsVerticalScrollIndicator={false}
+          removeClippedSubviews={true}
+        >
+        </FlatList>
+        {/* <SwipeListView
           showsVerticalScrollIndicator={false}
           data={data}
           renderItem={({ item }) => <Item_wishlist dulieu={item}/>}
@@ -49,7 +49,7 @@ const Favorite = (props) => {
           )}
           rightOpenValue={-10}
         /> */}
-        </View>
+      </View>
     </SafeAreaView>
   )
 }
@@ -57,79 +57,79 @@ const Favorite = (props) => {
 export default Favorite
 
 const styles = StyleSheet.create({
-    container:{
-        padding:15,
-        width:SIZES.width,
-        flexDirection:'column',
-        backgroundColor:COLOR.white,
-        justifyContent:'center'
-    },
-    wishlist:{
-        fontStyle:'normal',
-        fontWeight:'600',
-        fontSize:25,
-        lineHeight:38,
-        color:'#000000'
-    },
-    wishlist_list:{
-        marginTop:33,
-        display:'flex',
-        flexDirection:'column',
-        alignItems:'flex-start',
-    },
-    
+  container: {
+    padding: 15,
+    width: SIZES.width,
+    flexDirection: 'column',
+    backgroundColor: COLOR.white,
+    justifyContent: 'center'
+  },
+  wishlist: {
+    fontStyle: 'normal',
+    fontWeight: '600',
+    fontSize: 25,
+    lineHeight: 38,
+    color: '#000000'
+  },
+  wishlist_list: {
+    marginTop: 33,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+  },
+
 })
 
 const data = [
-    {
-        id: '1',
-        title:'Khám phá thời tiết đảo Jeju và kinh nghiệm du lịch chi tiết',
-        start:4.8,
-        view:100,
-        image:'https://ik.imagekit.io/tvlk/blog/2023/05/17VAvUV1-image.png?tr=dpr-2,w-675',
-        days:'2 ngay 1 dem',
-        price:10,
-        address:'songlong'
-    },
-    {
-        id: '2',
-        title:'Khám phá thời tiết đảo Jeju và kinh nghiệm du lịch chi tiết',
-        start:4.8,
-        view:100,
-        image:'https://ik.imagekit.io/tvlk/blog/2023/05/17VAvUV1-image.png?tr=dpr-2,w-675',
-        days:'2 ngay 1 dem',
-        price:10,
-        address:'songlong'
-    },
-    {
-        id: '3',
-        title:'Khám phá thời tiết đảo Jeju và kinh nghiệm du lịch chi tiết',
-        start:4.8,
-        view:100,
-        image:'https://ik.imagekit.io/tvlk/blog/2023/05/17VAvUV1-image.png?tr=dpr-2,w-675',
-        days:'2 ngay 1 dem',
-        price:10,
-        address:'songlong'
-    },
-    {
-        id: '4',
-        title:'Khám phá thời tiết đảo Jeju và kinh nghiệm du lịch chi tiết',
-        start:4.8,
-        view:100,
-        image:'https://ik.imagekit.io/tvlk/blog/2023/05/17VAvUV1-image.png?tr=dpr-2,w-675',
-        days:'2 ngay 1 dem',
-        price:10,
-        address:'songlong'
-    },
-    {
-        id: '5',
-        title:'Khám phá thời tiết đảo Jeju và kinh nghiệm du lịch chi tiết',
-        start:4.8,
-        view:100,
-        image:'https://ik.imagekit.io/tvlk/blog/2023/05/17VAvUV1-image.png?tr=dpr-2,w-675',
-        days:'2 ngay 1 dem',
-        price:10,
-        address:'songlong'
-    },
-    
+  {
+    id: '1',
+    title: 'Khám phá thời tiết đảo Jeju và kinh nghiệm du lịch chi tiết',
+    start: 4.8,
+    view: 100,
+    image: 'https://ik.imagekit.io/tvlk/blog/2023/05/17VAvUV1-image.png?tr=dpr-2,w-675',
+    days: '2 ngay 1 dem',
+    price: 10,
+    address: 'songlong'
+  },
+  {
+    id: '2',
+    title: 'Khám phá thời tiết đảo Jeju và kinh nghiệm du lịch chi tiết',
+    start: 4.8,
+    view: 100,
+    image: 'https://ik.imagekit.io/tvlk/blog/2023/05/17VAvUV1-image.png?tr=dpr-2,w-675',
+    days: '2 ngay 1 dem',
+    price: 10,
+    address: 'songlong'
+  },
+  {
+    id: '3',
+    title: 'Khám phá thời tiết đảo Jeju và kinh nghiệm du lịch chi tiết',
+    start: 4.8,
+    view: 100,
+    image: 'https://ik.imagekit.io/tvlk/blog/2023/05/17VAvUV1-image.png?tr=dpr-2,w-675',
+    days: '2 ngay 1 dem',
+    price: 10,
+    address: 'songlong'
+  },
+  {
+    id: '4',
+    title: 'Khám phá thời tiết đảo Jeju và kinh nghiệm du lịch chi tiết',
+    start: 4.8,
+    view: 100,
+    image: 'https://ik.imagekit.io/tvlk/blog/2023/05/17VAvUV1-image.png?tr=dpr-2,w-675',
+    days: '2 ngay 1 dem',
+    price: 10,
+    address: 'songlong'
+  },
+  {
+    id: '5',
+    title: 'Khám phá thời tiết đảo Jeju và kinh nghiệm du lịch chi tiết',
+    start: 4.8,
+    view: 100,
+    image: 'https://ik.imagekit.io/tvlk/blog/2023/05/17VAvUV1-image.png?tr=dpr-2,w-675',
+    days: '2 ngay 1 dem',
+    price: 10,
+    address: 'songlong'
+  },
+
 ]
