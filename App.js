2700529fd63b7c527bd59ce7e21633fcdd44bbec
lhhdai06022003 <lhhd06022003@gmail.com>
2023-10-31 23:12:14 +0700

    update tuor

--- App.js
+++ App.js
@@ -68,7 +68,7 @@ const App = () => {
     <Provider store={Store}>
       <NavigationContainer>
         <Stack.Navigator
-          initialRouteName={"Login"}
+          initialRouteName={"BottomTab"}
           screenOptions={{ headerShown: false }}>
           <Stack.Screen name="Login" component={Login} />
           <Stack.Screen name="Register" component={Register} />
--- constant/AxiosIntance.js
+++ constant/AxiosIntance.js
@@ -2,11 +2,7 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 const AxiosIntance = (contentType = 'application/json') => {
     const axiosInstance = axios.create({
-<<<<<<< HEAD
         baseURL: 'http://192.168.1.7:3000/'
-=======
-        baseURL: 'http://192.168.1.4:3000/'
->>>>>>> origin/VoThanhThe
     });
     axiosInstance.interceptors.request.use(
         async config => {
