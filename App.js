c733da10a3b0e6defc151d819713a8f85b570437
tuananh2k321 <haizzj123@gmail.com>
2023-11-16 21:49:07 +0700

    TA update branch tuan

--- App.js
+++ App.js
@@ -61,15 +61,15 @@ const App = () => {
     <Provider store={Store}>
       <NavigationContainer>
         <Stack.Navigator
-          initialRouteName={"BottomTab"}
+          initialRouteName={"Login"}
           screenOptions={{ headerShown: false }}>
-          {/* <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Login" component={Login} />
           <Stack.Screen name="Register" component={Register} />
           <Stack.Screen name="ForgetPassword" component={ForgetPassword} />
           <Stack.Screen name="NewPassword" component={NewPassword} />
           <Stack.Screen name="VerifyCode" component={VerifyCode} />
           <Stack.Screen name="VerifyCode2" component={Verifycode2} />
-          <Stack.Screen name="Successfully" component={Successfully} /> */}
+          <Stack.Screen name="Successfully" component={Successfully} /> 
           <Stack.Screen name="BottomTab" component={BottomTab} />
         </Stack.Navigator>
       </NavigationContainer>
